a157a1e2af4ddf3b10c44642ca892437dffd644c
ayushgit12 <ayush2005raj@gmail.com>
2024-05-23 22:41:26 +0530

    final commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import '../App.css'
 
 
 function card(data) {
@@ -15,7 +16,7 @@ function card(data) {
   
   
   return (
-    <div className={`bg-cyan-200 w-96 justify-center shadow-md rounded-md shadow-cyan-500 ${a!=4?'hover:scale-105':''} transition-all cursor-pointer ${a===4?'scale-110 bottom-1/4 right-1/3 z-10 fixed':''}`} onClick={popup}>
+    <div className={`bg-cyan-200 w-96 justify-center shadow-md rounded-md shadow-cyan-500 ${a!=4?'hover:scale-105':''} transition-all cursor-pointer ${a===4?'w-full trans absolute z-20':''}`} onClick={popup}>
       <div className="title bg-slate-700 text-white rounded-md p-3 h-24 flex items-center justify-center">{data.data.title}</div>
       <p className='text-right p-3'> {(data.data.author=='unknown')?'- Anonymous':'- '+data.data.author}</p>
       {<img className='m-auto h-52' src={(data.data.urlToImage)?data.data.urlToImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU3609aQ4Pn_5zYLUTBFrPQHntOdzbSMHpoXNn0Bm3Wg&s"} alt="" />}
